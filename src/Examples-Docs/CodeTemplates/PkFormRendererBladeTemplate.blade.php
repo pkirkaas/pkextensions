1a6d60e6e738c3468978d486cda30195e2d00564
Paul Kirkaas <pkirkaas@gmail.com>
2016-11-11 07:10:51 -0800

    Adding to code templates - really, just examples, for subform renderer

--- src/Examples-Docs/CodeTemplates/PkFormRendererBladeTemplate.blade.php
+++ src/Examples-Docs/CodeTemplates/PkFormRendererBladeTemplate.blade.php
@@ -1,10 +1,35 @@
 <?php
-/** Blade Template */
+/** Blade Template, with PkHtmlRenderer & PkMultiSubformRenderer - for scrolling subforms (Cart=>items) */
 use PkExtensions\Models\PkModel;
 use App\Models\Client;
 use PkExtensions\PkHtmlRenderer;
+use PkExtensions\PkMultiSubformRenderer;
+use App\References\DiagnosisRef;
+
 $out = new PkHtmlRenderer();
 $infoout = new PkHtmlRenderer();
+
+$diagnoses = $client->diagnoses;
+$diagrows = [];
+foreach ($diagnoses as $diagnosis) {
+  $diagrows[]= ['diagnosiscode_id' => $diagnosis->diagnosiscode_id,'id'=>$diagnosis->id];
+}
+$diagsf = new PkMultiSubformRenderer([
+    'basename'=>'diagnoses',
+    'tpl_fields' =>  ['id', 'diagnosiscode_id'],
+  ]);
+//Test adding attributes
+$diagsf->append_atts('create_button','tst-create-btn-class');
+$diagsf->append_atts('deletable_dataset','tst-dds-class');
+$diagsf->append_atts('js_template',['class'=>'tst-add-class-arr']);
+
+$diagsf->subform_data = $diagrows;
+$diagsf->hidden('id');
+//$diagsf->text('diagnosiscode_id');
+$diagsf->select('diagnosiscode_id',DiagnosisRef::getSelectList(true,true));
+
+
+
 $out[] =  PkForm::model($client);
 $infoout->wrap([
     'value' => PkForm::text('fname',null,['placeholder'=>'First Name']),
@@ -25,6 +50,7 @@ $infoout->wrap([
       'wrapperAttributes' => 'col-xs-4 tpm-wrapper',
     ]);
 $out[] = $infoout;
+$out[]=$diagsf;
 
 $out[] =   PkForm::button('Submit', ['type'=>'submit', 'name'=>'submit','value'=>'submit',
       'class'=>'pkmvc-button block fullwidth','title'=>"Save Changes"]);
