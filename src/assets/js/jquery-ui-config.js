29f88b2f6d76a810ae5cdacaba046b4692c162e5
Paul Kirkaas <pkirkaas@gmail.com>
2017-05-03 03:55:43 -0700

    Starting another time jquery plugin - not working yet

--- src/assets/js/jquery-ui-config.js
+++ src/assets/js/jquery-ui-config.js
@@ -21,6 +21,20 @@ $('body').on('focus', 'input.datepicker.auto-attach', function (e) {
       yearRange: '1930:2020'
   });
 });
+/** Better Initialize Time Picker */
+$('body').on('focus', 'input.timepicker.auto-attach', function (e) {
+    $(this).timepicker({
+       timeFormat: 'HH:mm:ss',
+    interval: 60,
+    minTime: '10',
+    maxTime: '6:00pm',
+    defaultTime: '11',
+    startTime: '10:00',
+    dynamic: false,
+    dropdown: true,
+    scrollbar: true
+  });
+});
 /** Persist current tab and return to it - but clear if doesn't exist in this
  * page
  */
