ab3c3a9199fb50e614adc5afcc381670c55f4843
Paul Kirkaas <pkirkaas@gmail.com>
2017-05-25 21:59:19 -0700

    Cleaning Master

--- src/Extensions/PkHtmlFacade.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PkExtensions;
-use Illuminate\Support\Facades\Facade;
-
-class PkHtmlFacade extends Facade {
-  protected static function getFacadeAccessor() {
-    return 'pkhtml';
-  }
-}
