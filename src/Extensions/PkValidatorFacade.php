0c41f7e76e821c41c2bc91fcdd3e089de2ac7e75
Paul Kirkaas <pkirkaas@gmail.com>
2017-05-25 22:03:32 -0700

    Cleaning Master

--- src/Extensions/PkValidatorFacade.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace PkExtensions;
-use Illuminate\Support\Facades\Facade;
-
-class PkValidatorFacade extends Facade {
-  protected static function getFacadeAccessor() {
-    return 'pkvalidator';
-  }
-}
