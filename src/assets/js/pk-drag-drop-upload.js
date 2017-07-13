d257ace646a9a19000014137ef6ecddde3e0920c
Paul Kirkaas <pkirkaas@gmail.com>
2017-07-13 05:38:50 -0700

    Added click to my jQuery uploader

--- src/assets/js/pk-drag-drop-upload.js
+++ src/assets/js/pk-drag-drop-upload.js
@@ -8,47 +8,88 @@
  * data-url: The url to post to, or '/ajax/upload' by default
  * data-name: The file-input name to use, or 'upload' by default
  */
+/*
+ $(".pk-dd-upload").ready( function() { 
+ var hiddenfileinput = $('<input type="file">');
+ });
+ */
+
 
 $(function () {
- $(".pk-dd-upload").on('dragenter', function (e){
-  e.preventDefault();
-  //$(this).css('background', '#BBD5B8');
- });
+  $(".pk-dd-upload").on('click', function (e) {
+    var me = e.target;
+    var hiddenfileinput = $('<input type="file" multiple>');
+    hiddenfileinput.on('change', function (ec) {
+      console.log("The type of files: "+typeof this.files);
+      if (!this.files instanceof FileList) {
+        return;
+      }
+      //var ifiles = this.files;
 
- $(".pk-dd-upload").on('dragover', function (e){
-  e.preventDefault();
- });
-
- $(".pk-dd-upload").on('drop', function (e){
-   var me = this;
-   var url = $(this).attr('data-url') || '/ajax/upload';
-   var finpname = $(this).attr('data-name') || 'upload';
-   var params = parseStr($(this).attr('data-params'));
-
-  $(this).css('background', '#D8F9D3');
-  e.preventDefault();
-  var files = e.originalEvent.dataTransfer.files;
-  var formData = new FormData();
-  formData.append(finpname, files[0]);
-
-  appendObjectToFormdata(formData,params);
-
-  $(this).addClass('ajax-loader');
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    contentType:false,
-    cache: false,
-    processData: false,
-    success: function(data){
-      VEventDispatcher.trigger('fileUploaded',"A new File");
-      //$(me).html(data);
-    }
-  }).done(function (data) {
-    $(this).removeClass('ajax-loader');
+      //console.log("The selected items:",this.files,'ifiles',ifiles);
+      /*
+       for (var i = 0, len = this.files.length; i < len; i++) {
+       console.log("File " + i, this.files[i], this.files.item(i));
+       }
+       */
+      //$(me).trigger('drop', this.files);
+      var onclick = 'onClk';
+      $(me).trigger('drop', [this.files, onclick]);
+    });
+    hiddenfileinput.trigger('click');
   });
- });
+
+  $(".pk-dd-upload").on('dragenter', function (e) {
+    e.preventDefault();
+  });
+
+  $(".pk-dd-upload").on('dragover', function (e) {
+    e.preventDefault();
+  });
+
+  $(".pk-dd-upload").on('drop', function (e, files, onclick) {
+    console.log("Entering drop; Files: onclick", onclick, files);
+    if (onclick) {
+      if (!files instanceof FileList) {
+        return;
+      }
+    } else {
+      files = e.originalEvent.dataTransfer.files;
+    }
+
+    var type = typeof files;
+    console.log("FILES: " + type, files);
+    var me = this;
+    var url = $(this).attr('data-url') || '/ajax/upload';
+    var finpname = $(this).attr('data-name') || 'upload';
+    var params = parseStr($(this).attr('data-params'));
+
+    e.preventDefault();
+    //var files = e.originalEvent.dataTransfer.files;
+    var formData = new FormData();
+    formData.append(finpname, files[0]);
+
+    appendObjectToFormdata(formData, params);
+
+    $(this).addClass('ajax-loader');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      contentType: false,
+      cache: false,
+      processData: false,
+      success: function (data) {
+        $(me).removeClass('ajax-loader');
+        VEventDispatcher.trigger('fileUploaded', "A new File");
+        //$(me).html(data);
+      }
+    }).done(function (data) {
+      $(me).removeClass('ajax-loader');
+    }).always(function (data) {
+      $(me).removeClass('ajax-loader');
+    });
+  });
 
 
 
