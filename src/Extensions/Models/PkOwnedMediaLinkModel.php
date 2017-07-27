ee501171807dae6050874150d5f5f79bcfe55055
Paul Kirkaas <pkirkaas@gmail.com>
2017-07-27 04:10:27 -0700

    Fix from migrating owned model functionality to trait

---
+++ src/Extensions/Models/PkOwnedMediaLinkModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace PkExtensions\Models;
+use PkExtensions\Traits\PkOwnedModelTrait;
+/**
+/**
+ * PkOwnedMediaLinkModel - If user's want to drag & drop a media link onto a Post
+ * or something - image, video, etc. Implements the "Owned" trait, so single model/
+ * table can belong to different kinds of owning models.
+ *
+ * @author pkirk
+ */
+class PkOwnedMediaLinkModel extends PkModel{
+  use PkOwnedModelTrait;
+  public static $table_field_defs = [
+      'url'=> ['type' => 'string'],
+      'element'=> ['type' => 'string','methods' => 'nullable'],
+      'display_type'=> ['type' => 'string','methods' => 'nullable'],
+      ];
+  
+  //protected $owner; #If set, the owning PkModel - make protected to call __get
+  public function __construct(array $atts = []) {
+    $atts = OwnedModelTraitConstruct($atts);
+    parent::__construct($atts);
+  }
+  public function save(Array $args = []) {
+    if (!filter_var($this->url, FILTER_VALIDATE_URL)) {
+      $this->delete();
+    }
+    return parent::save( $args);
+  }
+}
---
+++ src/Extensions/Traits/PkOwnedModelTrait.php
@@ -0,0 +1,124 @@
+<?php
+namespace PkExtensions\Traits;
+/**
+ * If some types of PkModels might be owned by several different other PkModels,
+ * instead of making separate classes & tables for them all, make a single class
+ * that can have different owner types
+ * 
+ * I've clevery extended PkModel to add the relationships hasOneTyped & hasManyTyped,
+ * so you can use them like any other relationship definition:
+ * 
+ * //In Owner of typed class:
+    public function avatar() {
+      return $this->hasOneTyped('avatar');
+    }
+
+ * @author pkirk
+ */
+trait PkOwnedModelTrait {
+
+  protected $owner; #If set, the owning PkModel - make protected to call __get
+  public static function getTableFieldDefsExtraOwnedModel() {
+    return [
+      #Like, 'App\Models\Profile'
+      'owner_model'=> ['type' => 'string', 'methods' => 'nullable'],
+      #Like - 7 - the ID of the owning model
+      'owner_id' => ['type' => 'integer', 'methods' => ['index', 'nullable']],
+      #Like 'avatar'; The "owner's" name for the attribute, in the relationship method
+      'att_name'=> ['type' => 'string', 'methods' => 'index'],
+
+      'category'=> ['type' => 'string', 'methods' =>  'nullable'],
+    ];
+  }
+  public function  OwnedModelTraitConstruct($atts) {
+    if (array_key_exists('owner', $atts)) {
+      $owner = $atts['owner'];
+      if (!$owner instanceOf PkModel) {
+        throw new PkException(["Owner was set, but not PkModel:", $owner]);
+      }
+      unset($atts['owner']);
+      $atts['owner_id'] = $owner->id;
+      $atts['owner_model']=get_class($owner);
+      $this->owner = $owner;
+    }
+    return $atts;
+  }
+  
+  public function getOwnerName() {
+    if (!($om = $this->owner_model)) {
+      return null;
+    }
+      $ucmodel = substr($om, strrpos($om, '\\') + 1)?:$om;
+      $ownername = strtolower($ucmodel);
+      return $ownername;
+  }
+
+  /** In case we create the upload object before we give it an owner
+   * 
+   * @param \PkExtensions\Models\PkModel $owner
+   * @return $this
+   */
+  public function setOwner(PkModel $owner) {
+    $this->owner_model = get_class($owner);
+    $this->owner_id = $owner->id;
+    $this->owner = $owner;
+    return $this;
+  }
+
+
+
+  /** So if it belongs to 'profile', allow $this->profile() & $this->profile to 
+   * return appropriately
+   * @param string $method
+   * @param array $args
+   * @return relationship/builder
+   */
+  public function __call($method, $args=[]) {
+    if ($method === $this->getOwnerName()) {
+      return $this->belongsTo($this->owner_model, 'owner_id');
+    }
+    return parent::__call($method, $args);
+  }
+
+  public function __get($att) {
+    if ($att === 'owner') {
+      if ($this->owner) {
+        return $this->owner;
+      }
+      $owner_model = $this->owner_model;
+      if (!$owner_model || !$this->owner_id) {
+        return null;
+      }
+      return $this->owner = $owner_model::find($this->owner_id);
+    }
+    if ($att === 'owner_model') {
+      return keyVal('owner_model',$this->attributes);
+    }
+    if ($att === $this->getOwnerName()) {
+      return $this->$att()->first();
+    }
+    return parent::__get($att);
+  }
+  
+
+  public function __set($att, $val) {
+    if (($att === 'owner') && ($val instanceOf PkModel) ) {
+      $this->owner_model = get_class($val);
+      $this->owner_id = $val->id;
+      $this->owner = $val;
+      return;
+    }
+    if ($att === $this->getOwnerName()) {
+      if (!$val) {
+        $this->owner_id = null;
+      } else if ($val instanceOf $this->owner_model) {
+        $this->owner_id = $val->id;
+      } else {
+        throw new PkException(
+            ["Wrong type Val; Owner-Model: {$this->owner_model}; Val:", $val]);
+      }
+    } else {
+      parent::__set($att, $val);
+    }
+  }
+}
