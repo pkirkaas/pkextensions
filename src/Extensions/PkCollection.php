ae70875c9f01ee4d6f45b31602a11b9c8b7c62a3
Paul Kirkaas <pkirkaas@gmail.com>
2018-10-29 04:06:05 -0700

    Big improvement on PkColllection - now can perform comparisons & filters o objects dynamic properties.

--- src/Extensions/PkCollection.php
+++ src/Extensions/PkCollection.php
@@ -5,6 +5,8 @@ namespace PkExtensions;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use PkExtensions\Models\PkModel;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 /**
  * PkCollection - just extends eloquent collection to delete 
@@ -28,8 +30,85 @@ class PkCollection extends Collection {
      return $retarr;
    }
 
+   /** Helper data_get only works on objects with set properties - not dynamic
+    * properties from __get, like models. So hijack for collection of PkModels
+    * @param type $target
+    * @param type $key
+    * @param type $default
+    */
+  public static function pkDataGet($target, $key,$default = null) {
+    if (is_null($key)) {
+      return $target;
+    }
+    $key = is_array($key) ? $key : explode('.', $key);
+    while (! is_null($segment = array_shift($key))) {
+      if ($segment === '*') {
+        if ($target instanceof Collection) {
+          $target = $target->all();
+        } elseif (! is_array($target)) {
+          return value($default);
+        }
+        $result = [];
+        foreach ($target as $item) {
+          $result[] = static::pkDataGet($item, $key);
+        }
+        return in_array('*',$key) ? Arr::collapse($result) : $result;
+      }
+
+      
+      if ($target instanceOf PkModel) {
+        $target = $target->$segment;
+      } else if (Arr::accessible($target) && Arr::exists($target, $segment)) {
+          $target = $target[$segment];
+      } elseif (is_object($target) && isset($target->{$segment})) {
+        $target = $target->{$segment};
+      } else {
+        return value($default);
+      }
+    }
+    return $target;
+  }
+ 
+/**
+  * Get a value retrieving callback.
+  * @param  string  $value
+  * @return callable - again, override for Models to use their custom properties
+  */
+    protected function valueRetriever($value) {
+        if ($this->useAsCallable($value)) {
+            return $value;
+        }
+        return function ($item) use ($value) {
+            return static::pkDataGet($item, $value);
+        };
+    } 
+
+
+
+    /**
+     * Get an operator checker callback.
+     *
+     * @param  string  $key
+     * @param  string  $operator
+     * @param  mixed  $value
+     * @return \Closure
+     */
+    protected function operatorForWhere($key, $operator = null, $value = null) {
+      $origOperatorForWhere = parent::operatorForWhere($key,$operator,$value);
+      return function($item) use ($key, $operator, $value, $origOperatorForWhere) {
+        $retrieved = static::pkDataGet($item,$key);
+        return $origOperatorForWhere;
+      };
+    }
+
+
+
+
+
    /**Takes assoc array of keys=>asc or desc
-    * and applies them to the collection
+    * and applies them t
+    * @param type $keyval
+    * @return typeo the collection
     */
    public function multiOrderby($keyval) {
      $collection = $this->toBase();
@@ -42,6 +121,17 @@ class PkCollection extends Collection {
      }
      return $collection;
    }
+   /** Just to give it the same signature as a model/builder 
+    * $key - what field to order by
+    * $desc = false or 'desc' - default ascending, else desc.
+    */
+   public function orderby($key, $desc = false) {
+      if ($desc === 'desc') {
+         return  $this->sortbyDesc($key);
+       } else {
+         return  $this->sortby($key);
+       }
+     }
 
    #### Only works for non-emtpy collections, of the same model...
    /** Returns a short string to allow recreation of the collection from 
