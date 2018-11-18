b9260aaa7f33bcf0063a18921e79a498449c16f8
Paul Kirkaas <pkirkaas@gmail.com>
2018-11-17 20:17:22 -0800

    The composer file thing doesn't work from symlinks

--- src/lib/require-external-files.php
+++ src/lib/require-external-files.php
@@ -1,3 +1,3 @@
 <?php
-require_once(__DIR__."/../../../../ccampbell/chromephp/ChromePhp.php");
+require_once(__DIR__."/../../../ccampbell/chromephp/ChromePhp.php");
 //require_once(__DIR__."/../../../../ccampbell/chromephp/ChromePhp.php");
