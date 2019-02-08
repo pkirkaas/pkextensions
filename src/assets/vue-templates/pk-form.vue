28bccf4f0bf10599cb1e7a9a3249e811aa59e989
Paul Kirkaas <pkirkaas@gmail.com>
2019-02-08 08:03:07 -0800

    About to reduce props & play with slots

--- src/assets/vue-templates/pk-form.vue
+++ src/assets/vue-templates/pk-form.vue
@@ -1,10 +1,17 @@
-<!--  A Basic form composable of components -
-    with say slots and custom directives -->
+<!--  A Basic form composable of components - based on Vue 2.6 Slots
+    with custom v-atts directive
+Usage:
+Included as a component with a single prop object "params"
+-->
+
+
+
+
 <template>
   <form  method="POST" class="pk-v-form" :class="form.class" v-atts="form.formatts">
     <input type='hidden' name="token" v-atts="form.tokenatts" :value="csrf">
     <div class="pk-v-form-inner" v-atts="form.frameatts">
-    <slot v-bind:formdata ="formdata" v-bind:componentdata="componentdata" v-bind:extraparams="extraparams">
+      <slot v-bind:formdata="formdata" v-bind:componentdata="componentdata" v-bind:extraparams="extraparams" v-bind:batts="batts" v-bind:dbatts="d">
 
 
     </slot>
@@ -16,13 +23,22 @@
 <script>
 
 export default {
-  props: ['form', 'formdata', 'componentdata', 'extraparams'],
+  //props: ['form', 'formdata', 'componentdata', 'extraparams','batts'],
+  props: {params: { form: {}, slotparams: {} },},
   name: 'pk-form',
   data: function() {
     return {
+      form: {},
       csrf: $('meta[name="csrf-token"]').attr('content'),
-    
     };
+  },
+  computed: {
+    form: function() {
+      return this.params.form;
+    },
+    slotparams: function() {
+      return this.params.slotparams;
+    },
   },
 };
 </script>
