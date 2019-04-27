545c154eb573ec5165e5951d38559754c9b5059c
Paul Kirkaas <pkirkaas@gmail.com>
2019-04-26 18:27:14 -0700

    Addedd include of SASSED stylesheet separately to the head partial for source mapping

--- src/Extensions/resources/views/system-partials/mix-layout-head.blade.php
+++ src/Extensions/resources/views/system-partials/mix-layout-head.blade.php
@@ -24,4 +24,5 @@ use PkExtensions\PopAtts;
      */?>
     <title>{{Config::get("app.name")}}</title>
     <link href="{{ mix('/mixed/css/stylesheets.css') }}" rel="stylesheet">
+    <link href="{{ mix('/mixed/css/sassed.css') }}" rel="stylesheet">
     {!!PopAtts::jsInit()!!}
