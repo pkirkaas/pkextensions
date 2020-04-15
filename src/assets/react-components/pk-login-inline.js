b0418d6230bd9730caae3c7a529385747c634826
Paul Kirkaas <pkirkaas@gmail.com>
2020-04-15 14:56:14 +0200

    Bad changes to pk-login-inline.js react component

--- src/assets/react-components/pk-login-inline.js
+++ src/assets/react-components/pk-login-inline.js
@@ -1,1 +1,20 @@
-console.log("inline-login");
+//import React, { Component } from '../../../../../../node-modules/react/umd/react.development.js';
+//import React, { Component } from '../../../../QuantumProfilesReact/node-modules/react/umd/react.development.js';
+import React, { Component } from 'react';
+//import ReactDOM from '../../../../../../node-modules/react-dom/react-dom';
+import ReactDOM from 'react-dom';
+
+//export default 
+class Example extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>The login component</h2>
+      </div>
+    );
+  }
+}
+
+if (document.getElementById('pk-login-inline')) {
+    ReactDOM.render(<pk-login-inline />, document.getElementById('pk-login-inline'));
+}
