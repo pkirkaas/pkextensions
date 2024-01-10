4e991e155d55720203b5c06f70857f798e63dbf6
Paul Kirkaas <pkirkaas@gmail.com>
2024-01-11 00:56:51 +0100

    Testing catching exceptions in ClearAll

--- src/Extensions/Console/Commands/ClearAll.php
+++ src/Extensions/Console/Commands/ClearAll.php
@@ -6,11 +6,22 @@ class ClearAll extends Command {
     protected $signature = 'wipe';
     protected $description = 'Runs all possible clears and cache dumps';
     public function handle() {
+      $clears = [ 'config:cache', 'cache:clear', 'view:clear', 'route:clear', 'clear-compiled',];
+      foreach ($clears as $clear) {
+        try {
+        $this->call($clear);
+        } catch (\Exception $e) {
+          $this->info("Clear exception: " . $e->getMessage());
+        }
+      }
+
+      /*
       $this->call('config:cache');
       $this->call('cache:clear');
       $this->call('view:clear');
       $this->call('route:clear');
       $this->call('clear-compiled');
+      */
       /*
       $output = "Pending...";
       $this->exec("composer dump-autoload", $output);
